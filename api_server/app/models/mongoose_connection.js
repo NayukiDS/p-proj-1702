d7657c847bee6ce895ed967108ff8f41aa41fd0e
Mamoru <jlchen2015@outlook.com>
2017-12-27 14:46:22 +0800

    add db connection info

---
+++ api_server/app/models/mongoose_connection.js
@@ -0,0 +1,9 @@
+var db_info = {
+    address: '39.106.184.215',
+    port: '27017',
+    db: 'yx_test',
+    user_name: 'user_test',
+    user_passwd: 'user_test_pd'
+};
+
+module.exports = db_info;
