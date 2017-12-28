7762e2a69c03f956c5f63bd5e6e63ad96f7b3833
Mamoru <jlchen2015@outlook.com>
2017-12-28 14:52:28 +0800

    change course schema p_student_id from array[]

--- api_server/app/models/course.js
+++ api_server/app/models/course.js
@@ -5,7 +5,7 @@ var CourseSchema = new Schema({
     _id: String,
     name: String,
     schedule: JSON,
-    p_student_id: [{ type: String, ref: 'user' }]
+    p_student_id: { type: String, ref: 'user' }
 });
 
 module.exports = mongoose.model('course', CourseSchema);
