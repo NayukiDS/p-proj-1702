6e9fc93a302d85410a4d6f71c6cde2ed2f99a6c1
Mamoru <jlchen2015@outlook.com>
2018-01-07 15:03:09 +0800

    change db to test server

--- api_server/app/models/mongoose_connection.js
+++ api_server/app/models/mongoose_connection.js
@@ -1,7 +1,9 @@
 var db_info = {
-    address: '39.106.184.215',
+    // address: '39.106.184.215',
+    address: '45.76.98.226',
     port: '27017',
-    db: 'yx_test',
+    // db: 'yx_test',
+    db: 'db_test',
     user_name: 'user_test',
     user_passwd: 'user_test_pd'
 };
