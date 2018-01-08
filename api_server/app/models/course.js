e3571b715eb4fe3ed4d18403eee5cf85bf543684
Mamoru <jlchen2015@outlook.com>
2018-01-08 11:30:19 +0800

    modify prop type of "schedule" in course schema

--- api_server/app/models/course.js
+++ api_server/app/models/course.js
@@ -7,7 +7,7 @@ var CourseSchema = new Schema({
     semester: { type: Number },
     professor_id: { type: String, ref: 'user' },
     student_r_id: { type: String, ref: 'user' },
-    schedule: [{ type: String }]//JSON string
+    schedule: [{ type: Schema.Types.Mixed }]//JSON string
 });
 
 module.exports = mongoose.model('course', CourseSchema);
