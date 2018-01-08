9a5df2f13867181c7a4632c86d5d11a43c84b22c
Mamoru <jlchen2015@outlook.com>
2018-01-08 11:34:25 +0800

    setting same structure as info model

--- api_server/app/models/schedule_json.js
+++ api_server/app/models/schedule_json.js
@@ -1,21 +1,26 @@
 function ScheduleJSON() {
-    this.type = null; //class lab lecture practical seminar
+    this.type = null; //class lab lecture practical seminar cancel
     this.day = null; //mo tu we th fr sa su
-    this.week = null; //{1,2,4,8,16...}
+    this.week = null; //[1,2,4,8,16...]
     this.time = {start: null, end: null};
     this.location = null;
-    this.instructor_id = null;
+    //this.instructor_id = null;
     var that = this;
+    this.setter = function (v_type, v_day, v_week, v_time, v_location) {
+        that.type = v_type;
+        that.day = v_day;
+        that.week = v_week;
+        that.time = v_time;
+        that.location = v_location;
+    };
     this.exportJSON = function () {
-        var json_obj = {
+        return {
             type: that.type,
             day: that.day,
             week: that.week,
             time: that.time,
-            location: that.location,
-            instructor_id: that.instructor_id
+            location: that.location
         };
-        return json_obj;
     }
 }
 
