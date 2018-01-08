728597151d877c86719addb8a02c78c685099bf5
Mamoru <jlchen2015@outlook.com>
2018-01-08 10:54:40 +0800

    add prop "pre_desk" to desk schema

--- api_server/app/models/desk.js
+++ api_server/app/models/desk.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var DeskSchema = new Schema({
     _id: { type: String, default: mongoose.Types.ObjectId().toString() },
+    pre_desk: { type: String },
     name: { type: String, default: '未命名桌面' },
     bind_class: { type: String, ref: 'class' },
     courses: [{ type: String, ref: 'course' }]
