99d02933a3a1d5fb2675920cad88a19b89cae6b2
Mamoru <jlchen2015@outlook.com>
2018-01-08 10:53:46 +0800

    remove prop "location" from course schema

--- api_server/app/models/course.js
+++ api_server/app/models/course.js
@@ -5,10 +5,9 @@ var CourseSchema = new Schema({
     _id: { type: String },
     name: { type: String, default: '未命名课程' },
     semester: { type: Number },
-    location: { type: String },
     professor_id: { type: String, ref: 'user' },
     student_r_id: { type: String, ref: 'user' },
-    schedule: [{ type:String }]//JSON string
+    schedule: [{ type: String }]//JSON string
 });
 
 module.exports = mongoose.model('course', CourseSchema);
