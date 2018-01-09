a34e54c5b74316fa4a9c8d04dec069d59162bc77
Mamoru <jlchen2015@outlook.com>
2018-01-10 00:24:46 +0800

    fix model require by "server.js"

--- api_server/server.js
+++ api_server/server.js
@@ -5,17 +5,18 @@ var express = require('express');
 var app = express();
 
 //----HTTPS
-// var options = {
-//     key: fs.readFileSync('./ssl/privkey.pem'),
-//     cert: fs.readFileSync('./ssl/fullchain.pem')
-// };
-//
-// https.createServer(options, app).listen(443, function () {
-//     console.log('Https server listening on port ' + 443);
-// });
+var options = {
+    key: fs.readFileSync('./ssl/privkey.pem'),
+    cert: fs.readFileSync('./ssl/fullchain.pem')
+};
+
+https.createServer(options, app).listen(443, function () {
+    console.log('Https server listening on port ' + 443);
+});
 //----HTTP
 var port = process.env.PORT || 80;
 app.listen(port);
+//----END
 
 var router = express.Router();
 
