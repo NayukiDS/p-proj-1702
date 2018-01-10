e70a2b9a0435c000f45a473dd0d41e9b91646620
Mamoru <jlchen2015@outlook.com>
2018-01-10 17:41:28 +0800

    add function  -get course list by week(alternative) in main "server.js"

--- api_server/server.js
+++ api_server/server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+var db_info = require('./app/models/mongoose_connection');
+mongoose.connect('mongodb://'
+    +db_info.user_name +':'
+    +db_info.user_passwd +'@'
+    +db_info.address+':'
+    +db_info.port+'/'
+    +db_info.db+'');
 //----HTTPS
 // var https = require('https')
 //     ,fs = require("fs");
@@ -32,3 +40,26 @@ router.route('/test_api')
     .get(function (req, res){
         res.json({info:"GET form Mamoru"});});
 
+var Course_list_get_by_week = require('./app/actions/course_list_get_by_week');
+router.route('/course_list')
+    .get(function (req, res) {
+        // var d_id = req.query.desk_id;
+        var d_id = "5a520da6d70e0138f4673fd0";
+        var day = req.query.activeIndex;
+        // var week = req.query.week;
+        var week = 10;
+        var course_get = new Course_list_get_by_week(d_id,week);
+        course_get.do_exec();
+        setTimeout(function () {
+            var res_json = course_get.getResult();
+            var day_int = parseInt(day);
+            // console.log(res_json[day_int]);
+            // if(day>=1&&day<=7){
+            if(day>=0&&day<=6){
+                // day_int -= 1;
+                res.json(res_json[day_int].courses);
+            }else{
+                res.status(500).json({info:"bad activeIndex request!"});
+            }
+        },50);
+    });
