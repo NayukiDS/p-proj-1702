215f611ced5795b361584ff9f4711029b850c04b
Mamoru <jlchen2015@outlook.com>
2018-01-12 17:33:26 +0800

    add new view model "view_desk_event"

---
+++ api_server/app/models/view_desk_events.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var View_desk_event = new Schema({
+    _id: String,
+    bind_class: String,
+    pre_desk: String,
+    course: [{ type:String }],
+    name: String,
+    event_docs: [{ type: Schema.Types.Mixed }]
+});
+
+module.exports = mongoose.model('view_desk_event',View_desk_event);
