76b2149a5c58685409f0da94018711414686ee27
Mamoru <jlchen2015@outlook.com>
2018-01-16 20:18:28 +0800

    add "Access-Control-Allow" to express add new router-semester

--- api_server/server.js
+++ api_server/server.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(express.static('res'));
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 
 var mongoose = require('mongoose');
 var db_info = require('./app/models/mongoose_connection');
@@ -41,7 +54,7 @@ router.route('/test_api')
         res.json({info:"GET form Mamoru"});});
 
 var Course_list_get_by_week = require('./app/actions/course_list_get_by_week');
-var date_sweek_convert = require('./app/actions/date_sweek_convert');
+var date_sweek_convert = require('./app/module/date_sweek_convert');
 router.route('/course_list')
     .get(function (req, res) {
         // var d_id = req.query.desk_id;
@@ -50,7 +63,7 @@ router.route('/course_list')
         console.log("req_date:"+date);
         var date_json = date_sweek_convert(date);
         if(!date_json.valid){
-            res.status(500).json({info:"invalid date."});
+            res.status(400).json({info:"invalid date."});
             return;
         }
         var course_get = new Course_list_get_by_week(d_id,date_json.semester,date_json.week);
@@ -75,4 +88,143 @@ router.route('/course_list')
                 res.json(res_obj);
             }
         }
+    });
+
+var event_list_get_by_sweek = require('./app/actions/event_list_get_by_sweek');
+router.route('/event_list')
+    .get(function (req, res) {
+        // var d_id = req.query.desk_id;
+        var d_id = "5a520da6d70e0138f4673fd0";
+        var date = req.query.date;
+        console.log("req_date:"+date);
+        var date_json = date_sweek_convert(date);
+        if(!date_json.valid){
+            res.status(400).json({info:"invalid date."});
+            return;
+        }
+        var event_get = new event_list_get_by_sweek(d_id,date_json.semester,date_json.week);
+        event_get.do_exec(rc);
+
+        function rc() {
+            var res_json = event_get.getResult();
+            var res_obj = {
+                semester: date_json.semester,
+                week: date_json.week,
+                day: date_json.day,
+                date: date_json.date,
+                event_list: []
+            };
+            var event_list = res_json[date_json.day];
+            if(event_list!==undefined){
+                res_obj.event_list = event_list.courses;
+                res.json(res_obj);
+                // res.json(course_list.courses);
+            }else {
+                // res.status(500).json([]);
+                res.json(res_obj);
+            }
+        }
+    });
+
+var comment_list_get_by_event = require('./app/actions/comment_list_get_by_event');
+var comment_delete = require('./app/actions/comment_delete');
+var comment_create = require('./app/actions/comment_create');
+router.route('/comment_list')
+    .get(function (req, res) {
+        var event_id = req.query.event_id;
+        if(!event_id){
+            res.status(400).json({info:"invalid event_id"});
+            return;
+        }
+        // var event_id = "1600231";
+        var comment_get = new comment_list_get_by_event(event_id);
+        comment_get.do_exec(rc);
+
+        function rc() {
+            var res_json = comment_get.getResult();
+            if(res_json.err){
+                res.status(500).json({info:res_json.err_msg});
+                return;
+            }
+            res.json(res_json.result);
+        }
+    })
+    .post(function (req, res) {
+        var pre_comment_id = req.query.pre_comment_id;
+        var user_id = req.query.user_id;
+        var event_id = req.query.event_id;
+        var content = req.query.content;
+
+        if(!pre_comment_id){
+            // res.status(400).json({info:"invalid pre_comment_id"});
+            // return;
+            pre_comment_id = "";
+        }
+        if(!user_id){
+            res.status(400).json({info:"invalid user_id"});
+            return;
+        }
+        if(!event_id){
+            res.status(400).json({info:"invalid event_id"});
+            return;
+        }
+        if(!content){
+            res.status(400).json({info:"invalid content"});
+            return;
+        }
+        // var event_id = "1600231";
+        var comment_new = new comment_create(pre_comment_id, user_id, event_id, content);
+        comment_new.do_exec(rc);
+
+        function rc() {
+            var res_json = comment_new.getResult();
+            if(res_json.err){
+                res.status(500).json({info:res_json.err_msg});
+                return;
+            }
+            res.json(res_json.result);
+        }
+    })
+    .delete(function (req, res) {
+        var comment_id = req.query.comment_id;
+        if(!comment_id){
+            res.status(400).json({info:"invalid comment_id"});
+            return;
+        }
+        // var event_id = "1600231";
+        var comment_get = new comment_delete(comment_id);
+        comment_get.do_exec(rc);
+
+        function rc() {
+            var res_json = comment_get.getResult();
+            if(res_json.err){
+                res.status(500).json({info
+                        :res_json.err_msg});
+                return;
+            }
+            res.json(res_json.result);
+        }
+    });
+
+router.route('/semester_list')
+    .get(function (req, res) {
+        var date_json = [
+            {
+                semester: 201601,
+                start_week: "2016/9/7"
+            },
+            {
+                semester: 201602,
+                start_week: "2017/2/22"
+            },
+            {
+                semester: 201701,
+                start_week: "2017/9/4"
+            },
+            {
+                semester: 201702,
+                start_week: "2018/3/26"
+            }
+        ];
+        res.json(date_json);
     });
