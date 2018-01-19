94a7540a3593f188816e2528e735c25652dce865
Mamoru <jlchen2015@outlook.com>
2018-01-19 10:58:57 +0800

    add res header small case "Content-Type" support

--- api_server/server.js
+++ api_server/server.js
@@ -11,6 +11,7 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1');
     res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("content-type", "application/json;charset=utf-8");
     next();
 });
 
