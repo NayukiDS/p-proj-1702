13577c1da5c721fe71ff4b15fadf4f8d4d0dcf50
Mamoru <jlchen2015@outlook.com>
2018-01-21 18:43:50 +0800

    new action-desk_new

---
+++ api_server/app/actions/desk_new.js
@@ -0,0 +1,76 @@
+var User = require('../models/user');
+var Desk = require('../models/desk');
+
+function desk_new(user_id, pre_desk, name, bind_class) {
+    var res_json = {
+        err: false,
+        err_msg: "",
+        status: 200,
+        result: ""
+    };
+    var that = this;
+    this.user_id = user_id;
+    this.pre_desk = pre_desk;
+    this.name = name;
+    this.bind_class = bind_class;
+    this.desk_create =function (user_id, pre_desk, name, bind_class, callback) {
+        var desk = new Desk;
+        if(!pre_desk){
+            pre_desk = "";
+        }else{
+            var promise = Desk.findById(pre_desk).exec();
+            promise.then(
+                function (result) {
+                    if(Object.keys(result).length === 0){
+                        res_json.err = true;
+                        res_json.err_msg = "Could not find pre_desk required by client.";
+                        res_json.status = 500;
+                        callback();
+                        return false;
+                    }
+                },
+                function (err) {
+                    res_json.err = true;
+                    res_json.err_msg = err;
+                    res_json.status = 500;
+                    callback();
+                    return false;
+                }
+            )
+        }
+        desk.pre_desk = pre_desk;
+        if(!name) name = "未命名桌面";
+        desk.name = name;
+        if(!bind_class) bind_class = "";
+        desk.bind_class = bind_class;
+        desk.courses = [];
+        var promise_save = desk.save();
+        promise_save.then(
+            function (result) {
+                console.log(result);
+                res_json.result = result;
+                callback();
+                return true;
+            },
+            function (err) {
+                res_json.err = true;
+                res_json.err_msg = err;
+                res_json.status = 500;
+                callback();
+                return false;
+            }
+        );
+    };
+    this.do_exec = function(callback){
+        that.desk_create(that.user_id, that.pre_desk, that.name, that.bind_class, callback);
+    };
+    this.getResult = function () {
+        return res_json;
+    };
+    this.test = function () {
+        var desk = new Desk;
+        desk.pre_desk
+    }
+}
+
+module.exports = desk_new;
--- api_server/app/models/desk.js
+++ api_server/app/models/desk.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DeskSchema = new Schema({
-    _id: { type: String, default: mongoose.Types.ObjectId().toString() },
-    pre_desk: { type: String },
-    name: { type: String, default: '未命名桌面' },
+    // _id: { type: String, default: mongoose.Types.ObjectId().toString() },
+    pre_desk: { type: String, default: "" },
+    name: { type: String, default: "未命名桌面" },
     bind_class: { type: String, ref: 'class' },
     courses: [{ type: String, ref: 'course' }]
 });
